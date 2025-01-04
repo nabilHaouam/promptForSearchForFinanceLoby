6fa353de4327b71ab478006e3332bab73eba9913
nabilHaouam <workmailnabil@gmail.com>
2025-01-04 10:31:23 +0100

    moved to port 5001

--- prompt.js
+++ prompt.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 4000;
+const port = 5001;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
